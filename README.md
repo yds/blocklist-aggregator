fb43ea2121a958c3ea4dbd7cf83b56b288c8d483
Yarema <yds@Necessitu.de>
2025-01-27 18:27:51 -0500

    added code and docs

---
+++ README.md
@@ -0,0 +1,144 @@
+# Blocklist IPrange compiler
+
+## What does it do?
+
+This is a set of [Python][]/[Invoke][] tasks to download a collection
+of public blacklists and public whitelists to be used in a firewall.
+
+## How to use it
+
+Pick a working directory where to install these [Invoke][] tasks.
+e.g. `/etc/iprange/` or `/var/db/ipf/`
+
+Install by cloning the repo:
+```sh
+git clone https://github.com/yds/blocklist-compiler /var/db/ipf
+```
+Change `/var/db/ipf/` in the above command to whatever directory
+makes sense to you on *your* system, this HowTo will assume that's
+the directory where these scripts are installed.
+
+Review and edit the `invoke.yml` configuration file:
+- `blocklist`: location of the `blocklist.cidr` file loaded by the packet filter.
+- `knowngood`: location of the `knowngood.cidr` file loaded by the packet filter.
+- `updatelog`: location of the `update.log` file for `suricata-update` output.
+- `iprep`: location of the [Suricata][] IP reputation directory.
+- `blacklists`: list of additional local blacklist CIDR files.
+- `whitelists`: list of additional local whitelist CIDR files.
+- `knowngoods`: list of public whitelists to download.
+- `blocklists`: list of public blacklists to download.
+- `zzz_disabled`: list of inactive, disabled list definitions stashed out of the way for reference.
+- `whitelist`: URL of a sample `whitelist.txt` file to download if a local file cannot be found.
+
+**IMPORTANT**: create a `whitelist.cidr` file with all the IP ranges
+which you **NEVER** want blocked.  Include [RFC1918][] address
+ranges and any IP address ranges assigned to you by your ISP.
+
+The `invoke.yml` configuration file has default settings to include
+any local `blacklist*.cidr` and `whitelist*.cidr` wildcard files
+it can find. Create or symlink any additional files fitting the
+wildcard naming pattern as needed.
+
+Edit `/etc/pf.conf` and add the tables and rules managed by the `pfreplace` task:
+```
+table <blocklist> counters persist file "/var/db/ipf/blocklist.cidr"
+table <knowngood> counters persist file "/var/db/ipf/knowngood.cidr"
+
+# example rdr using the <blocklist> and <knowngood> tables with OpenBSD's spamd(8)
+rdr pass on wan0 proto tcp from { <blocklist> <spamd> !<knowngood> !<spamd-white> } to port smtp -> (lo0:0) port spamd
+
+# example block rule to drop all <blocklist> attack traffic
+block drop in quick on wan0 from <blocklist> label "block attack traffic"
+```
+
+Edit and install the `crontab` file to `/usr/local/etc/cron.d/blocklist`:
+```crontab
+*/5	*	*	*	*	root	cd /var/db/ipf && /usr/local/bin/invoke pfreplace
+31	0	*	*	*	root	cd /var/db/ipf && /usr/local/bin/invoke suricataupdate
+```
+Skip `suricataupdate` in your crontab if not needed. `pfreplace`
+can be replaced with `fetch` to download and compile the blacklists
+and whitelists without loading them into the packet filter.
+
+The `fetch` task is smart enough to not download any public lists
+more often than the `interval` setting allows. Some blacklists are
+updated as often as every 5 minutes [NUBI][], or every hour
+[CINS Army][], but most are updated no more often than once a day.
+
+It does not make sense to run the `fetch` task more often than every
+5 minutes.  Running less often than every 5 minutes is fine.
+
+## [Invoke][] tasks
+
+All tasks have an `--verbose` parameter to display the output of
+what is getting done.  The `clean` task _requires_ the `--verbose`
+parameter to actually delete all the files and directories created
+by the other tasks.
+
+- `inv config --verbose`: displays the loaded configuration.
+- `inv fetch --verbose`: downloads the IP reputation lists.
+- `inv spf2cidr --verbose`: looks up the DNS TXT/SPF records
+  for all the senders in the `whitelist.spf` file to add to
+  the `knowngood.cidr` output.
+- `inv whitelist --verbose`: process the `whitelist.txt` file
+  to a CIDR list. This file can be a mix of hostnames,
+  IP addresses or CIDRs.
+- `inv pfreplace --verbose`: `pfctl` replace the <knowngood>
+  and <blocklist> tables with new CIDRs.
+- `inv suricataupdate --verbose`: compile the downloaded
+  IP reputation lists into [Suricata][] IP reputation format
+  CSV files and perform a full [Suricata][] update.
+- `inv clean --verbose`: delete all the files created for
+  compiling the output CIDR lists.
+
+DNS lookups tend to be slow therefore `whitelist.spf` and `whitelist.txt`
+are processed only when the timestamp of the files changes.  
+
+## Requirements and dependencies
+
+On [FreeBSD][] the following ports/pkgs are required:
+- [lang/python3](https://Python.org/ "Python is a programming language that lets you get shite done!"): Meta-port for the Python interpreter 3.x
+- [devel/py-invoke](https://PyInvoke.org/ "Invoke is a Python library for managing shell-oriented subprocesses and organizing executable Python code into CLI-invokable tasks."): Python task execution tool and library
+- [devel/py-fabric](https://FabFile.org/ "Fabric is a high level Python library designed to execute shell commands remotely over SSH."): High level SSH command execution
+- [devel/py-pyyaml](https://PyYAML.org/ "YAML Ain't Markup Language"): Python YAML parser
+- [net-mgmt/iprange](https://GitHub.com/firehol/iprange "IP ranges management tool"): IP ranges management tool
+
+The following ports/pkgs are optional:
+- [security/suricata](https://Suricata.io/ "High Performance Network IDS, IPS and Security Monitoring engine"): High Performance Network IDS, IPS and Security Monitoring engine
+- [ftp/curl](https://cURL.se/ "Command line tool and library for transferring data with URLs"): Command line tool and library for transferring data with URLs
+
+## Linux
+
+To use [curl](https://cURL.se/ "Command line tool and library for transferring data with URLs")
+instead of [FreeBSD][]'s `fetch(1)` add the following line to `invoke.yml`:
+```yaml
+fetch: /usr/local/bin/curl -Rso
+```
+adjust the path above to `/usr/bin/curl` and with a few other path
+tweaks the `fetch` and `suricataupdate` tasks should work on Linux.
+
+## Misc Notes
+
+The sample `whitelist.txt` file is downloaded from the [MalTrail][]
+malicious traffic detection system project.  Definitely look over
+the content and remove anything you do not need whitelisted.
+
+Before enabling the <blocklist> in the packet filter run `inv fetch`
+and ensure that everything you need whitelisted is indeed in the
+`knowngood.cidr` output file and search the generated `blocklist.cidr`
+file to ensure nothing you need whitelisted ends up in the <blocklist>.
+
+## License
+
+See [LICENSE](https://GitHub.com/yds/blocklist-compiler/blob/master/LICENSE "BSD3CLAUSE").
+
+[FreeBSD]:https://FreeBSD.org/ "The Power to Serve"
+[PyYAML]:http://www.PyYAML.org/ "YAML Ain't Markup Language"
+[Python]:https://Python.org/ "Python is a programming language that lets you get shite done!"
+[Invoke]:https://PyInvoke.org/ "Invoke is a Python library for managing shell-oriented subprocesses and organizing executable Python code into CLI-invokable tasks."
+[Fabric]:https://FabFile.org/ "Fabric is a high level Python library designed to execute shell commands remotely over SSH."
+[Suricata]:https://Suricata.io/ "High Performance Network IDS, IPS and Security Monitoring engine"
+[MalTrail]:https://GitHub.com/stamparm/maltrail "Maltrail is a malicious traffic detection system"
+[RFC1918]:https://www.RFC-Editor.org/rfc/rfc1918 "Address Allocation for Private Internets"
+[CINS Army]:https://CINSarmy.com/list-download/ "Collective Intelligence Network Security"
+[NUBI]:https://www.NUBI-Network.com/faq.php "NUBI was designed to be a replacement for the venerable BadIPs.com after their website went offline in late 2020."
---
+++ tasks.py
@@ -0,0 +1,249 @@
+'''Blocklist IPrange aggregator
+
+Copyright © 2024-2025, Yarema <yds@Necessitu.de>
+
+This software is open source.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the name of the organization nor the names of its
+    contributors may be used to endorse or promote products derived from this
+    software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+'''
+from fabric import task
+from invoke import Collection, Exit
+from dns.resolver import resolve, NXDOMAIN
+from urllib.request import urlopen
+from socket import inet_aton
+from calendar import timegm
+from time import strptime, time
+import yaml
+import sys
+import os
+import re
+
+ns = Collection()
+cwd = os.getcwd()
+encoding = 'utf-8'
+verbose = {'verbose': 'show running tasks'}
+
+def aton(addr):
+    return inet_aton(addr.split('/')[0])
+
+#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#
+@task(help={'verbose': 'show discovered paths'})
+def config(c, verbose=False):
+    '''configure tasks environment'''
+    if 'git' not in c: c.git = c.run('which git', hide=True, warn=True).stdout.strip()
+    if 'sed' not in c: c.sed = c.run('which sed', hide=True, warn=True).stdout.strip() + ' -Ee'
+    if 'curl' not in c: c.curl = c.run('which curl', hide=True, warn=True).stdout.strip() + ' -Rso'
+    if 'fetch' not in c: c.fetch = c.run('which fetch', hide=True, warn=True).stdout.strip() + ' -qo'
+    if 'pfctl' not in c: c.pfctl = c.run('which pfctl', hide=True, warn=True).stdout.strip()
+    if 'iprange' not in c: c.iprange = c.run('which iprange', hide=True, warn=True).stdout.strip() + ' --optimize'
+    if 'suricataupdate' not in c: c.suricataupdate = c.run('which suricata-update', hide=True, warn=True).stdout.strip() + ' --no-test'
+    if 'updatelog' not in c: c.updatelog = '/var/log/suricata/update.log'
+    if 'iprep' not in c: c.iprep = '/usr/local/etc/suricata/iprep/'
+    if 'knowngood' not in c: c.knowngood = '/var/db/knowngood.cidr'
+    if 'blocklist' not in c: c.blocklist = '/var/db/blocklist.cidr'
+    if 'whitelists' not in c: c.whitelists = ['/var/db/whitelist*.cidr']
+    if verbose:
+        sys.stdout.write(yaml.dump(dict(c), default_flow_style=False, width=999, Dumper=yaml.CDumper))
+ns.add_task(config)
+
+@task(config, default=True, help=verbose)
+def fetch(c, verbose=False):
+    '''Fetch IP Reputation lists'''
+    def fetch(lists, cidr, include=True):
+        datadir = f'{cwd}/pass/' if include else f'{cwd}/drop/'
+        os.makedirs(f'{datadir}', 0o755, exist_ok=True)
+        for l in lists:
+            lst = f"{datadir}{l['name']}.cidr"
+            url = l['url']
+            mtime = 60 * 60 * 24
+            if 'interval' in l:
+                mtime = str(l['interval'])
+                if mtime.endswith('w'):     # Weeks
+                    mtime = int(mtime[:-1]) * 60 * 60 * 24 * 7
+                elif mtime.endswith('d'):   # Days
+                    mtime = int(mtime[:-1]) * 60 * 60 * 24
+                elif mtime.endswith('h'):   # Hours
+                    mtime = int(mtime[:-1]) * 60 * 60
+                elif mtime.endswith('m'):   # Minutes
+                    mtime = int(mtime[:-1]) * 60
+                else:                       # Seconds
+                    mtime = int(mtime)
+            if os.path.isfile(lst):
+                mtime += os.path.getmtime(lst) - 30
+            if time() > mtime:
+                try:
+                    if l['format'] == 'cidr':
+                        c.run(f'{c.fetch} {lst} {url}', echo=verbose, pty=True)
+                        if 'date' in l:
+                            tag = f"# {l['date']['label']}"
+                            fmt = l['date']['format']
+                            with open(lst) as lst:
+                                for mtime in lst:
+                                    if mtime.startswith(tag):
+                                        mtime = timegm(strptime(mtime.split(':',1)[1].strip(), fmt))
+                                        os.utime(lst.name, (mtime, mtime))
+                                        break
+                    elif l['format'] == 'json':
+                        data = yaml.load(urlopen(url).read().decode(encoding), Loader=yaml.CSafeLoader)
+                        addrs = {}
+                        table = data[l['table']] if 'table' in l else data[l['ipv4']]
+                        for r in table:
+                            if 'services' in l:
+                                if r['service'] in l['services']:
+                                    addr = r[l['ipv4']].strip()
+                                    sep = '\t' if len(addr) > 15 else '\t\t'
+                                    addrs[addr] = f"{sep}# {r['service']}"
+                            else:
+                                addrs[r.strip()] = ''
+                        with open(lst, 'w') as lst:
+                            for addr in sorted(addrs.keys(), key=aton):
+                                print(f'{addr}{addrs[addr]}', file=lst)
+                        if 'date' in l:
+                            tag = l['date']['label']
+                            fmt = l['date']['format']
+                            mtime = timegm(strptime(data[tag], fmt))
+                            os.utime(lst.name, (mtime, mtime))
+                    elif l['format'] == 'regex':
+                        data = urlopen(url).read().decode(encoding).split()
+                        addrs = []
+                        regex = re.compile(l['re'])
+                        for r in data:
+                            r = regex.match(r)
+                            if r:
+                                addrs.append(r.group(1))
+                        with open(lst, 'w') as lst:
+                            for addr in sorted(addrs, key=aton):
+                                print(addr, file=lst)
+                except:
+                    continue
+        include = ' ' if include else f' --except {c.knowngood} '
+        c.run(f"{c.iprange} {datadir}*.cidr{include}{' '.join(c.whitelists)} > {cidr}", echo=verbose, pty=True)
+    spf2cidr(c, verbose)
+    whitelist(c, verbose)
+    fetch(c.knowngoods, c.knowngood, True)
+    fetch(c.blocklists, c.blocklist, False)
+ns.add_task(fetch)
+
+@task(config, help={'verbose': 'show SPF records and lookup errors'})
+def spf2cidr(c, verbose=False):
+    '''SPF hostnames to CIDR'''
+    addrs = {}
+    def resolvespf(host):
+        try:
+            answers = resolve(f'{host}.', 'TXT')
+        except NXDOMAIN as e:
+            if verbose:
+                print(e, file=sys.stderr)
+            return
+        for rdata in answers:
+            for spf in rdata.strings:
+                spf = spf.decode(encoding)
+                if spf.startswith('v=spf1 '):
+                    if verbose:
+                        sep = '\t' if len(host) > 15 else '\t\t'
+                        print(f'{host}:{sep}{spf}')
+                    for addr in spf.split():
+                        if addr.startswith('ip4:') and '.' in addr:
+                            addr = addr[4:].rstrip('.')
+                            if len(addr.split('.')) > 2:
+                                if len(addr.split('.')) == 3:
+                                    addr = f'{addr}.0/24'
+                                elif addr.endswith('/32'):
+                                    addr = addr.split('/')[0]
+                                addrs[addr] = host
+                        elif addr.startswith('include:'):
+                            resolvespf(addr[8:])
+                        elif addr.startswith('redirect='):
+                            resolvespf(addr[9:])
+    hosts = f'{cwd}/whitelist.spf'
+    if os.path.isfile(hosts):
+        os.makedirs(f'{cwd}/pass', 0o755, exist_ok=True)
+        mtime = os.path.getmtime(hosts)
+        cidr = f'{cwd}/pass/SPF.cidr'
+        if not os.path.isfile(cidr) or os.path.getmtime(cidr) < mtime:
+            with open(hosts) as hosts:
+                for host in hosts:
+                    resolvespf(host.strip())
+            with open(cidr, 'w') as cidr:
+                for addr in sorted(addrs.keys(), key=aton):
+                    sep = '\t' if len(addr) > 15 else '\t\t'
+                    print(f'{addr}{sep}# {addrs[addr]}', file=cidr)
+            os.utime(cidr.name, (mtime, mtime))
+ns.add_task(spf2cidr)
+
+@task(config, help=verbose)
+def whitelist(c, verbose=False):
+    '''whitelist.txt with IPs and/or hostnames to CIDR'''
+    lst = f'{cwd}/whitelist.txt'
+    if not os.path.isfile(lst) and 'whitelist' in c:
+        c.run(f'{c.fetch} {lst} {c.whitelist}', echo=verbose, pty=True)
+    if os.path.isfile(lst):
+        os.makedirs(f'{cwd}/pass', 0o755, exist_ok=True)
+        mtime = os.path.getmtime(lst)
+        cidr = f'{cwd}/pass/WhiteList.cidr'
+        if not os.path.isfile(cidr) or os.path.getmtime(cidr) < mtime:
+            with open(cidr, 'w') as cidr:
+                hide = not verbose
+                for addr in c.run(f'{c.iprange} {lst} 2>/dev/null', echo=verbose, hide=hide, warn=True, pty=True).stdout.split():
+                    if addr not in ('0.0.0.0','255.255.255.255'):
+                        print(addr, file=cidr)
+            os.utime(cidr.name, (mtime, mtime))
+ns.add_task(whitelist)
+
+@task(fetch, help=verbose)
+def pfreplace(c, verbose=False):
+    '''Repalce PF tables'''
+    quiet = '' if verbose else ' -q'
+    c.run(f'{c.pfctl}{quiet} -t knowngood -T replace -f {c.knowngood}', echo=verbose, pty=True)
+    c.run(f'{c.pfctl}{quiet} -t blocklist -T replace -f {c.blocklist}', echo=verbose, pty=True)
+ns.add_task(pfreplace)
+
+@task(config, help=verbose)
+def suricataupdate(c, verbose=False):
+    '''Update IP Reputation lists'''
+    DROP = 0
+    PASS = 1
+    SCORE = 100
+    os.makedirs(f'{c.iprep}', 0o755, exist_ok=True) # /usr/local/etc/suricata/iprep/
+    c.whitelists.append(c.knowngood)
+    c.run(f"{c.iprange} {cwd}/pass/*.cidr {' '.join(c.whitelists)} | {c.sed} 's/^(.*)/\\1,{PASS},{SCORE}/' > {c.iprep}pass.list", echo=verbose, pty=True)
+    c.run(f"{c.iprange} {cwd}/drop/*.cidr --except {' '.join(c.whitelists)} | {c.sed} 's/^(.*)/\\1,{DROP},{SCORE}/' > {c.iprep}drop.list", echo=verbose, pty=True)
+    c.run(f"{c.suricataupdate} >{c.updatelog} 2>/dev/null", echo=verbose, pty=True)
+ns.add_task(suricataupdate)
+
+@task(config, help={'verbose': 'show deleted CIDRs'})
+def clean(c, verbose=False):
+    '''Delete all CIDRs'''
+    rmfr = f'rm -vfr {cwd}/drop {cwd}/pass'
+    if verbose:
+        c.run(rmfr, echo=verbose, pty=True)
+    else:
+        sys.exit(f'\n\tRun `inv clean --verbose` to delete all CIDRs for real:\n\n\t{rmfr}\n')
+ns.add_task(clean)
+
+#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#===#
